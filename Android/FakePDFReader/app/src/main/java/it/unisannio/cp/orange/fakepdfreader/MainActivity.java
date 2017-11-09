c3031ee0fc07fe46d3e90b666a26924681449604
Raffaele Mignone <raffy.m.96@gmail.com>
2017-11-09 12:15:13 +0100

    FakeODFReader action-filter

---
+++ Android/FakePDFReader/app/src/main/java/it/unisannio/cp/orange/fakepdfreader/MainActivity.java
@@ -0,0 +1,13 @@
+package it.unisannio.cp.orange.fakepdfreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
