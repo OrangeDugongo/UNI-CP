fea2b3e51077a31c38db07b546f7c54967a026eb
Raffaele Mignone <raffy.m.96@gmail.com>
2017-12-01 14:30:16 +0100

    copyright

--- Android/Score30/app/src/main/java/it/unisannio/cp/orange/score30/activity/BugReportActivity.kt
+++ Android/Score30/app/src/main/java/it/unisannio/cp/orange/score30/activity/BugReportActivity.kt
@@ -1,3 +1,22 @@
+/*
+ *      Copyright (c) 2017 Raffaele Mignone
+ *
+ *      This file is part of  Score30
+ *
+ *      Score30 is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      Score30 is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with Score30.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.unisannio.cp.orange.score30.activity
 
 import android.app.DatePickerDialog
